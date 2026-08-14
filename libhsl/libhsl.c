5afb02c3f585fb85a4c77ce0b0c20ca67632fe6e
Alexis Montoison <Alexis.Montoison@amd.com>
2026-08-13 20:05:44 -0600

    Release 4.0.8

--- libhsl/libhsl.c
+++ libhsl/libhsl.c
@@ -3,3 +3,9 @@
 bool LIBHSL_isfunctional(){
   return false;
 }
+
+void LIBHSL_version(int *major, int *minor, int *patch){
+    *major = LIBHSL_VER_MAJOR;
+    *minor = LIBHSL_VER_MINOR;
+    *patch = LIBHSL_VER_PATCH;
+}
