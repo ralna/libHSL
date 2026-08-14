3241c8afe76b0809a2c56476328d61a47330df76
Alexis Montoison <Alexis.Montoison@amd.com>
2026-08-13 20:05:44 -0600

    Release 4.0.8

--- libhsl/libhsl.h
+++ libhsl/libhsl.h
@@ -1,8 +1,22 @@
 #include <stdbool.h>
 
 #ifndef _LIBHSL_H_
-#define _LIBHSL_H_ 
+#define _LIBHSL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 bool LIBHSL_isfunctional();
+
+/* Version number of LIBHSL */
+#define LIBHSL_VER_MAJOR 4
+#define LIBHSL_VER_MINOR 0
+#define LIBHSL_VER_PATCH 8
+void LIBHSL_version(int *major, int *minor, int *patch);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  /* _LIBHSL_H_ */
